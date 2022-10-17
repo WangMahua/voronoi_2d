7bde1b9b0419fcf39a7d1a45c97d7fec9a64d723
WangMahua <whole9177j53@gmail.com>
2022-10-17 17:01:18 +0800

    first commit

--- agent/agent.m
+++ agent/agent.m
@@ -2,28 +2,15 @@ classdef agent
  % 3 UAV property (camera/bearing/range sensor)  
 
     properties
-
-        agent_number = 3;
-
         % state of 3 UAV
         pos = zeros(3,3);
         last_pos = zeros(3,3);
         vel = zeros(3,3);
         last_vel = zeros(3,3);
+        alpha = [1.8 1.9 1.3];
+        k = [1.1 1 0.5];
 
-        % param of camera 
-        fx = 565.6;
-        fy = 565.6;
-
-        % param of range 
-        cov_r_x = 0.1;
-        cov_r_y = 0.1;
-        cov_r_z = 0.1;
-
-        % param of bearing 
-        cov_b_alpha = 0.1;
-        cov_b_beta = 0.1;
-
+        agent_number = 3;
     end
     
    methods (Static)
@@ -32,6 +19,8 @@ classdef agent
         % camera
         function dense = dense_function_1(q_pos,u_pos)
 
+            alpha = 1.8;
+            k = 1.1;
             % uav pos 
             px = u_pos(1);  py = u_pos(2);
             % particle pos      
@@ -41,20 +30,29 @@ classdef agent
         end
 
         % bearing
-        function dense = dense_function_2()
-            dense = 1/cov_r_x^2 + 1/cov_r_y^2 + 1/cov_r_z^2;
+        function dense = dense_function_2(q_pos,u_pos)
+            
+            alpha = 1.9;
+            k = 1;
+            % uav pos 
+            px = u_pos(1);  py = u_pos(2);
+            % particle pos      
+            qx = q_pos(1);  qy = q_pos(2);
+
+            dense = (((px-qx)*(px-qx) + (py-qy)*(py-qy)));
         end
 
         % range sensor    
-        function dense = dense_function_3(q_p,p_p)
+        function dense = dense_function_3(q_pos,u_pos)
             
-            % p -> uav pos ; q -> particle pos  
-            r = q_p - p_p;
-       
+            alpha = 1.3;
+            k = 0.5;
+            % uav pos 
+            px = u_pos(1);  py = u_pos(2);
+            % particle pos      
+            qx = q_pos(1);  qy = q_pos(2);
 
-            dense = 1/(cov_b_beta*(r(1)^2 + r(2)^2)) + 1/(cov_b_alpha*(r(1)^2 + r(2)^2 + r(3)^2)) + ...
-                 1/(cov_b_beta*(r(1)^2 + r(2)^2)) + 1/(cov_b_alpha*(r(1)^2 + r(2)^2 + r(3)^2)) +...
-                 1/(cov_b_beta*(r(1)^2 + r(2)^2)) + 1/(cov_b_alpha*(r(1)^2 + r(2)^2 + r(3)^2)) ;
+            dense =(((px-qx)*(px-qx) + (py-qy)*(py-qy)));
         end
    end
 
